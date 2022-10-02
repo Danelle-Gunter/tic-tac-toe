d22e37028e46066a94929479c00d37203190f639
Danelle <rhowia@gmail.com>
2022-10-02 16:13:14 -0400

    Used html and css to display the game board

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+const Player = (name, symbol) => {
+    // What is under the player goes here
+    return {name, symbol};
+};
+
+const gameBoard = (() => {
+    // stuff that happens to game goes here
+    const board = [];
+    return {board};
+})();
+
+const displayController = (() => {
+    // displaying everything goes here
+
+})();
